5ad571a44542225518a25a0a62799950ababadcf
Main <main@wireless-165-124-198-91.nuwlan.northwestern.edu>
2013-01-29 11:25:10 -0600

    First commit, really

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Wedloom::Application.config.session_store :cookie_store, key: '_wedloom_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Wedloom::Application.config.session_store :active_record_store
