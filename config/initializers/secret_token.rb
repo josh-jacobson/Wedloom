30aa6bff47d39ffd2f4c54360f296267243d870f
Josh Jacobson <joshjacobson14@gmail.com>
2013-02-07 19:58:38 -0600

    Everything rolled into one app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testapp::Application.config.secret_token = '512069b75ac3ef5c26ebc56bb449b2e40ad4d21b35d6efd66350ceea265bd1abfd37195855453231592f44270b200a4da36b324c69832a1e064605d4ffee0847'
---
+++ test/functional/static_pages_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionController::TestCase
+  test "should get home" do
+    get :home
+    assert_response :success
+  end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+  end
+
+  test "should get budget" do
+    get :budget
+    assert_response :success
+  end
+
+end
