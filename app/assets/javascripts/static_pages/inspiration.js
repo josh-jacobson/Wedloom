7a317280b715491dce9c341dfe416ae721c36967
Josh Jacobson <joshjacobson14@gmail.com>
2013-03-04 00:08:07 -0600

    everything in its right place

---
+++ app/assets/javascripts/static_pages/inspiration.js
@@ -0,0 +1,29 @@
+    $(document).ready(function() {
+      
+      // Generate the grid layout using the freetile jQuery plugin
+      $('#container').freetile({
+        animate: true,
+        elementDelay: 30
+      });
+
+      // Event handling for hovering over images: 
+      // 1. make button invisible
+      $(".roll").css("opacity","0");
+ 
+      // 2. Make button appear when mouse hovers over image
+      $(".roll").hover(function () {
+        $(this).stop().animate({opacity: 1}, "slow");
+      },
+         
+      // 3. Make button disappear when mouse leaves image
+      function () {
+          $(this).stop().animate({opacity: 0}, "slow");
+      });
+
+      // Event handling for button click
+      $(".addtoloom").click(function() {
+        alert("Added to your loom.");
+        this.parent.append('<p>Hello!</p>');
+      });
+
+    });
