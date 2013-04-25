38be7e2598502bd4f6bcfa1f36ac3cec372c6473
Josh Jacobson <joshjacobson14@gmail.com>
2013-04-25 13:22:46 -0500

    buttons on your loom photos

--- app/assets/javascripts/customers/loom.js
+++ app/assets/javascripts/customers/loom.js
@@ -3,4 +3,18 @@ $(document).ready(function() {
     animate: true,
     elementDelay: 30
   });
+
+    // Event handling for hovering over images: 
+    // 1. make button invisible
+    $(".roll").css("opacity","0");
+ 
+    // 2. Make button appear when mouse hovers over image
+    $(".roll").hover(function () {
+      	$(this).stop().animate({opacity: 1}, "slow");
+    },
+         
+    // 3. Make button disappear when mouse leaves image
+    function () {
+        $(this).stop().animate({opacity: 0}, "slow");
+    });
 });
