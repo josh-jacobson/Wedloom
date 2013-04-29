0fb5577bba1aa9aaf777e5e7ab894187010358a7
Josh Jacobson <joshjacobson14@gmail.com>
2013-04-29 18:39:44 -0500

    Load more images with AJAX

--- app/assets/javascripts/static_pages/inspiration.js
+++ app/assets/javascripts/static_pages/inspiration.js
@@ -1,30 +1,38 @@
-    $(document).ready(function() {
-      
-      // Generate the grid layout using the freetile jQuery plugin
-      $('#container').freetile({
-        animate: true,
-        elementDelay: 30
-      });
+$(document).ready(function() {
 
-      // Event handling for hovering over images: 
-      // 1. make button invisible
-      $(".roll").css("opacity","0");
- 
-      // 2. Make button appear when mouse hovers over image
-      $(".roll").hover(function () {
-        $(this).stop().animate({opacity: 1}, "slow");
-      },
-         
-      // 3. Make button disappear when mouse leaves image
-      function () {
-          $(this).stop().animate({opacity: 0}, "slow");
-      });
+  // Generate the grid layout using the freetile jQuery plugin
+  $('#container').freetile({
+    animate: true,
+    elementDelay: 30
+  });
 
-      // Event handling for button click
-      $(".addtoloom").click(function() {
+  // Event handling for hovering over images: 
+  // 1. make button invisible
+  $(".roll").css("opacity","0");
 
-      });
+  // 2. Make button appear when mouse hovers over image
+  $(".roll").hover(function () {
+    $(this).stop().animate({opacity: 1}, "slow");
+  },
+     
+  // 3. Make button disappear when mouse leaves image
+  function () {
+      $(this).stop().animate({opacity: 0}, "slow");
+  });
 
-      $(".alert").alert()
+  // Event handling for button click
+  $(".addtoloom").click(function() {
 
+  });
+
+  $(".alert").alert()
+
+  $('a.loadmore').click(function() {
+    $('#container').freetile({
+      animate: true,
+      elementDelay: 30
     });
+  });
+    
+
+});
