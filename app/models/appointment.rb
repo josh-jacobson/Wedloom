473cd65ad2d37e9687b9cee4bffe2f916fa1e747
Josh Jacobson <joshjacobson14@gmail.com>
2013-05-03 20:10:25 -0500

    Central time zone

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,4 +2,9 @@ class Appointment < ActiveRecord::Base
   attr_accessible :customer_id, :planner_id, :appointment_time
   belongs_to :customer
   belongs_to :planner
+
+  before_create :set_time_to_now
+  def set_time_to_now
+    self.appointment_time = Time.now
+  end
 end
